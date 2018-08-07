990f27fdc9acd79582a46263119106b839d2e39c
pugazhvananU <40732664+pugazhvananU@users.noreply.github.com>
2018-08-07 11:08:57 +0530

    Create ex4.java

---
+++ ex4.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.lang.*;
+
+class ex4{
+	 int length;  
+	 int width;  
+	  
+	 void insert(int l,int w){  
+	  length=l;  
+	  width=w;  
+	 }  
+	  
+	 void calculateArea(){System.out.println(length*width);}  
+	  
+	 public static void main(String args[]){  
+    ex4 r1=new ex4();
+	  ex4 r2=new ex4();
+	  
+	  r1.insert(11,5);  
+	  r2.insert(3,15);  
+	  
+	  r1.calculateArea();  
+	  r2.calculateArea();  
+	}  
+	}  
