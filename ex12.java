81bc6172950674f1a8ef4da087ad2ef6f046abcf
pugazhvananU <40732664+pugazhvananU@users.noreply.github.com>
2018-08-07 11:40:17 +0530

    Create ex12.java

---
+++ ex12.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+	class ex12{  
+	int id;  
+	String name;  
+	  
+	void display(){System.out.println(id+" "+name);}  
+	  
+	public static void main(String args[]){  
+	ex12 s1=new ex12();  
+    ex12 s2=new ex12();
+	s1.display();  
+	s2.display();  
+	}  
+	}  
