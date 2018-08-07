7d752a3cd9d818d87c3abe914128f6d47e560539
pugazhvananU <40732664+pugazhvananU@users.noreply.github.com>
2018-08-07 11:36:04 +0530

    Create ex11.java

---
+++ ex11.java
@@ -0,0 +1,8 @@
+import java.util.*;
+import java.lang.*;
+	class ex11{  
+	ex11(){System.out.println("ex11 is created");}  
+	public static void main(String args[]){  
+	ex11 b=new ex11();  
+	}  
+	}  
