d367c1fde6757da87733eac321b08418e07bf1d7
pugazhvananU <40732664+pugazhvananU@users.noreply.github.com>
2018-08-07 11:24:27 +0530

    Create ex7.java

---
+++ ex7.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+
+	class ex7{  
+	  void sum(int a,int b){System.out.println(a+b);}  
+	  void sum(int a,int b,int c){System.out.println(a+b+c);}  
+	  
+	  public static void main(String args[]){  
+      ex7 obj=new ex7();  
+	  obj.sum(10,10,10);  
+	  obj.sum(20,20);  
+  
+	  }  
+	}  
