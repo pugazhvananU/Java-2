9fac0b2918871272f2ff49e09740bb62621fa02b
pugazhvananU <40732664+pugazhvananU@users.noreply.github.com>
2018-08-07 10:50:28 +0530

    Create ex2.java

---
+++ ex2.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+class ex2
+    {  
+	 int id;//data member (also instance variable)  
+	 String name;//data member(also instance variable)  
+	  
+	 public static void main(String args[])
+     {  
+	  Student1 s1=new Student1();//creating an object of Student  
+	  System.out.println(s1.id);  
+	  System.out.println(s1.name);  
+     }  
+	}  
